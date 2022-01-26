2263af6fb58a429f2f5e3466797d20ab000d6e38
Andreas Paul <xorpaul@gmail.com>
2022-01-26 13:42:23 +0100

    init

---
+++ manifests/init.pp
@@ -0,0 +1,25 @@
+# g10k_testmodule_submodule::init
+# A description of what this class does
+#
+# @summary A short summary of the purpose of this class
+#
+# @param parameter1
+#   Explanation of what this parameter affects.
+#
+# @example Simple use
+#   include g10k_testmodule_submodule::init
+#
+# @example Use with params
+#   class { 'g10k_testmodule_submodule::init':
+#     $parameter1 => 'undef',
+#   }
+#
+# @author Andreas Paul <xorpaul@gmail.com>
+#
+# @note Copyright 26.01.2022 Andreas Paul
+#
+class g10k_testmodule_submodule::init(
+  String $parameter1 = undef,
+) {
+
+}
